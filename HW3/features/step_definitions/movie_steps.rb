7872b7083aec10a1757c78115b9e518317e974c3
jstol <jakestolee@gmail.com>
2013-08-14 21:20:10 -0700

    Completed part 1:  Background step for 'Given the following movies exist' adds specified movies to the database.

--- HW3/features/step_definitions/movie_steps.rb
+++ HW3/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create movie
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
